206ccf35d7ecc7723f5af6b223a5666d6d5dd004
Todor Mitskovski <todorbm85@gmail.com>
2019-01-28 14:31:57 +0200

    Add Unlock-all-files script

---
+++ unlock-allFiles.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    Unlocks all files and/or directories recursively for a given path or file path.
+.EXAMPLE
+    PS C:\> unlock-allFiles.ps1 -path "C:\docs"
+    PS C:\> unlock-allFiles.ps1 -path "C:\temp.dat"
+.INPUTS
+    $path: The path to the file or directory is mandatory
+.NOTES
+    The script requires the presense of the sysinternals handle tool in the same directory. https://docs.microsoft.com/en-us/sysinternals/downloads/handle
+#>
+Param(
+    [Parameter(Mandatory=$true)]$path
+)
+
+$handlesList = "& `".\handle.exe`" $path"
+$pids = New-Object -TypeName System.Collections.ArrayList
+$handlesList | ForEach-Object { 
+    $isFound = $_ -match "^.*pid: (?<pid>.*?) .*$"
+    if ($isFound) {
+        $id = $Matches.pid
+        if (-not $pids.Contains($id)) {
+            $pids.Add($id) > $null
+        }
+    }
+}
+
+$pids | ForEach-Object {
+    if (-not $_) {
+        return    
+    }
+
+    Get-Process -Id $_ | % {
+        $date = [datetime]::Now
+        "$date : Forcing stop of process Name:$($_.Name) File:$($_.FileName) Path:$($_.Path) `nModules:$($_.Modules)"
+    }
+}
